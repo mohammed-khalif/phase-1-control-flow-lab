2971c29ffbfb7b1f8100a670e2171d4f2a238d51
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-03-16 09:48:45 -0400

    completed my assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+  }
+  return result;
 }
